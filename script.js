682df62a1eaff2b036cea09afe7390c23f54db05
Luke Hook <lukehook@Lukes-MacBook-Pro.local>
2021-09-22 17:07:13 -0400

    Made the function to play a round and determine the winner

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ const playChoice = [ 'rock', 'paper', 'scissors' ];
 
 // create function for computer to randomly return a choice from the array
 function computerPlay() {
-  computerChoice = playChoice[Math.floor(Math.random() * 3)];
-  console.log(computerChoice);
+  return playChoice[Math.floor(Math.random() * 3)];
 }
 
 // create a function for user to play either rock, paper, or scissors
+const playerSelection = prompt('Choose either rock, paper, or scissors');
+const computerSelection = computerPlay();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    console.log("It's a tie!");
+  }
+  else if (
+    (playerSelection.toLowerCase() === 'rock' &&
+      computerSelection.toLowerCase() === 'scissors') ||
+    (playerSelection.toLowerCase() === 'paper' &&
+      computerSelection.toLowerCase() === 'rock') ||
+    (playerSelection.toLowerCase() === 'scissors' &&
+      computerSelection.toLowerCase() === 'paper')
+  ) {
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  }
+  else {
+    console.log(
+      `You Lose! ${playerSelection.toUpperCase()} loses to ${computerSelection.toUpperCase()}`
+    );
+  }
+}
+playRound(playerSelection, computerSelection);
